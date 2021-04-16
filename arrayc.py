6dd9cbc5b248931f664c5f2e9b0155bbd618515c
Fexxyi <theartman380@gmail.com>
2021-04-16 15:38:17 +0400

    v0.2

--- arrayc.py
+++ arrayc.py
@@ -7,7 +7,7 @@ import abc
 
 
 
-_types_ = {
+_py2c_types = {
     int:     ctypes.c_longlong,
     float:   ctypes.c_longdouble,
     str:     ctypes.c_wchar_p,
@@ -16,11 +16,15 @@ _types_ = {
     None:    ctypes.c_void_p,
 }
 
+_c2py_types = {
+    ...
+}
+
 
 class ArraycIterator:
     def __init__(self, arrayc_object):
         self.index = 0
-        self.stop = arrayc_object.length_
+        self.stop = arrayc_object.length
         self.arrayc_object = arrayc_object
     
     def __iter__(self):
@@ -35,48 +39,11 @@ class ArraycIterator:
         return item
 
 
-class AttributeProtector:
-    def __init__(self, name):
-        self.name = name
-        self._array_initialized = False
-    
-    @property
-    def array_initialized(self):
-        return self._array_initialized
-    
-    @array_initialized.setter
-    def array_initialized(self, value):
-        if self._array_initialized:
-            error_message = "Array object is initlized."
-            raise ValueError(error_message)
-        
-        if not isinstance(value, bool):
-            error_message = "Only logical object can be assigned"
-            raise TypeError(error_message)
-        
-        self._array_initialized = value
-
-    def __get__(self, instance, instance_type=None):
-        return instance.__dict__[self.name]
-
-    def __set__(self, instance, value):
-        if not self.array_initialized:
-            instance.__dict__[self.name] = value
-            self.array_initialized = True
-        else:
-            raise AttributeError("can't set attribute")
-
-    def __delete__(self, instance):
-        raise AttributeError("can't delete attribute") 
-
-
 class BaseArray(abc.ABC):
     
     # Just stubs.
-    _length_ = 101
-    _type_ = ctypes.c_longlong
-    length_ = _length_
-    type_ = _type_
+    length = 101
+    arrtype = ctypes.c_void_p
 
     @abc.abstractmethod
     def __setitem__(self, key, value, /):
@@ -91,8 +58,8 @@ class BaseArray(abc.ABC):
     # Methods.
     def __repr__(self):
         iterable = self.tolist()
-        arrtype = "ctypes.%s" % self.type_.__name__
-        length = self.length_
+        arrtype = "ctypes.%s" % self.arrtype.__name__
+        length = self.length
         repr_text = "arrayc(iterable=%s, arrtype=%s, length=%s)" % (
             iterable,
             arrtype,
@@ -109,7 +76,7 @@ class BaseArray(abc.ABC):
         return ArraycIterator(self)
     
     def __len__(self):
-        return self.length_
+        return self.length
 
     def tolist(self):
         return list(self)
@@ -119,21 +86,19 @@ class BaseArray(abc.ABC):
             self[i] = j
     
     def updateitems(self, iterable):
-        if len(iterable) > self.length_:
+        if len(iterable) > self.length:
             error_message = ("Array length is - {0}, iterable length is - {1}, " + \
-                "it's uneven!").format(self.length_, len(iterable))
+                "it's uneven!").format(self.length, len(iterable))
             raise ValueError(error_message)
-        arrtype_type = self.type_
+        # Checks if the call will raise an exception.
+        arrtype = self.arrtype
         for item in iterable:
-            if not isinstance(item, arrtype_type):
-                error_message = ("All elements of the iteration must be of" + \
-                    "the same type as the elements of the array. " + \
-                    "array type is '{0}'").format(arrtype_type.__name__)
-                raise TypeError(error_message)
+            # Then it was not the wrong value for iterable.
+            arrtype(item.value if isinstance(item, arrtype) else item)
         self.setitems(iterable)
 
     def clear(self, /):
-        arrtype_obj = self.type_()
+        arrtype_obj = self.arrtype()
         for i in range(len(self)):
             self[i] = arrtype_obj
      
@@ -150,13 +115,13 @@ class BaseArray(abc.ABC):
     
     def pop(self, index, /):
         value = self[index]
-        self[index] = self.type_()
+        self[index] = self.arrtype()
         return value
 
     def remove(self, value, /):
         list_obj = self.tolist()
         index = list_obj.index(value)
-        self[index] = self.type_()
+        self[index] = self.arrtype()
 
     def reverse(self, /):
         list_obj = self.tolist()
@@ -169,7 +134,7 @@ class BaseArray(abc.ABC):
         self.setitems(list_obj)
 
 
-def create_array(iterable=[], arrtype=ctypes.c_void_p, length=101):
+def create_array(iterable, arrtype, length):
     name = '_Array'
     bases = (ctypes.Array,)
     attrs = {
@@ -183,19 +148,34 @@ def create_array(iterable=[], arrtype=ctypes.c_void_p, length=101):
 
 class Arrayc(BaseArray):
 
-    type_ = AttributeProtector('_type_')
-    length_ = AttributeProtector('_length_')
-    _arrayobject = AttributeProtector('_arrayobject')
-
     def __init__(self, iterable=[], arrtype=ctypes.c_void_p, length=101):
+        self._is_setattr_initialized = True
         array_object = create_array(iterable, arrtype, length)
-       
-        self._length_ = array_object._length_
-        self._type_ = array_object._type_
+
+        self.length = array_object._length_
+        self.arrtype = array_object._type_
         self._arrayobject = array_object
-       
         self._setitem = array_object.__setitem__
         self._getitem = array_object.__getitem__
+        
+        self._is_setattr_initialized = False
+    
+
+    def __setattr__(self, name, value):
+        initialized = '_is_setattr_initialized'
+        if name == initialized or bool(getattr(self, initialized, False)):
+            super(Arrayc, self).__setattr__(name, value)
+        else:
+            error_message = "'{0}' object attribute '{1}' is read-only".format(
+                self.__class__.__name__, name
+            )
+            raise AttributeError(error_message)
+    
+    def __delattr__(self, name):
+        error_message = "'{0}' object attribute '{1}' is read-only".format(
+            self.__class__.__name__, name
+        )
+        raise AttributeError(error_message)
     
     def __setitem__(self, key, value, /):
         return self._setitem(key, value)
@@ -204,24 +184,7 @@ class Arrayc(BaseArray):
         return self._getitem(key)
     
     def __delitem__(self, key, /):
-        self[key] = self.type_()
-
-    @property
-    def length_(self):
-        return self._length_
-
-    @property
-    def type_(self):
-        return self._type_
-
-    @property
-    def arrayobject(self):
-        return self._arrayobject
-    
-    @arrayobject.setter
-    def arrayobject(self, new_arrayc):
-        if not isinstance(new_arrayc, self.arrayobject.__class__):
-            error_message = "New array "
+        self[key] = self.arrtype()
     
     @staticmethod
     def setarrayitems(array_, iterable):
@@ -229,19 +192,34 @@ class Arrayc(BaseArray):
 
     def getexpanded(self, length=21, *, is_arrayc=True):
         if not isinstance(length, int):
-            error_message = "Length is integer parameter not - '{0}'".format(
+            error_message = "length is integer parameter not - '{0}'".format(
                     length.__class__.__name__)
             raise TypeError(error_message)
-        arrtype = self.type_
-        newlength = self.length_ + length
+        if length < 0:
+            raise ValueError("param 'length' >= 0")
+        if not isinstance(is_arrayc, bool):
+            error_message = "'is_arrayc' is 'bool' object not '{0}'".format(
+                is_arrayc.__class__.__name__)
+            raise TypeError(error_message)
+        arrtype = self.arrtype
+        newlength = self.length + length
         if is_arrayc:
             array_object = Arrayc(self, arrtype, newlength)
         else:
             array_object = create_array(self, arrtype, newlength)
         return array_object
     
-    #def expand(self, length):
-      #  new_array_object = self.getexpanded(length, is_arrayc=True)
+    def expand(self, length=21):
+        self._is_setattr_initialized = True
+        try:
+            array_object = self.getexpanded(length, is_arrayc=False)
+            self.length += length
+            self._arrayobject = array_object
+            self._setitem = array_object.__setitem__
+            self._getitem = array_object.__getitem__
+            
+        finally:
+            self._is_setattr_initialized = False
 
 
 def equaltypes(x, y):
@@ -282,8 +260,8 @@ def arrayc(iterable=[], arrtype=None, length=-1):
     if not is_static:
         error_message = "Object types are not equal."
         raise TypeError(error_message)
-    if arrtype in _types_:
-        arrtype = _types_[arrtype]
+    if arrtype in _py2c_types:
+        arrtype = _py2c_types[arrtype]
     if length == -1:
         length = len(iterable)
     # Check arrtype param.
@@ -294,17 +272,4 @@ def arrayc(iterable=[], arrtype=None, length=-1):
     # Create Arrayc object.
     arrayc_object = Arrayc(iterable, arrtype, length)
     return arrayc_object
-
-
-"""
-class arraycf:
-    def __init__(self, arrtype=ctypes.c_long, length=101, iterable=[]):
-        name = 'Array'
-        bases = (ctypes.Array, BaseArray, self.__class__)
-        attrs = {
-            '_length_': length,
-            '_type_': arrtype,
-        }
-
-        Arraycf = type(name, bases, attrs)
-"""
+    
